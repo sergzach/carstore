3592211d2bb6f2601d15d8aab0a0dfb43ffb96d9
Sergey <sergzach@gmail.com>
2019-05-12 18:00:33 +0300

    Minor fixes

--- run/server.sh
+++ run/server.sh
@@ -3,5 +3,5 @@
 CUR_DIR=$(dirname "$0")
 
 cd $CUR_DIR/../app && \
-carserver.py	--max-connections=32 --buff-size=4096 \
-				--response-module=carstore_app --max-read-time=2000 --poll_timeout=1
+carserver.py --max-connections=32 --buff-size=4096 \
+--response-module=carstore_app --max-read-time=2000 --poll_timeout=1
